d4302e2a8f4b1a2cfaa913da4379cfaee9cd0ea3
Padraig Donnelly <pdonnelly19@qub.ac.uk>
2016-01-14 11:12:53 +0000

    Added comments.

--- tutorials/simple_swhe.py
+++ tutorials/simple_swhe.py
@@ -2,9 +2,15 @@ import random
 
 
 def generate_key(security_parameter):
+    """ Generate a random n bit key, where n is the secuity parameter.
+
+    :param security_parameter:
+    :return: a random n bit integer, where n is the security parameter
+    """
     key_length = security_parameter ** 2
     key = random.getrandbits(key_length)
 
+    # Key needs to be an odd number.
     if key % 2 == 0:
         key += 1
 
@@ -12,6 +18,13 @@ def generate_key(security_parameter):
 
 
 def encrypt_bit(security_parameter, key, bit):
+    """ Encrypt the passed-in bit.
+
+    :param security_parameter:
+    :param key:
+    :param bit: a single-bit
+    :return: a ciphertext corresponding to the passed-in bit
+    """
     random_bits = random.getrandbits(security_parameter)
     while random_bits % 2 != bit % 2:
         random_bits = random.getrandbits(security_parameter)
@@ -20,14 +33,19 @@ def encrypt_bit(security_parameter, key, bit):
 
 
 def decrypt(ciphertext, key):
+    """ Decrypt the passed-in ciphertext using the passed-in key
+
+    :param ciphertext: Ciphertext to be decrypted
+    :param key: Key with which to decrypt the ciphertext
+    :return: Plaintext corresponding to the passed-in ciphertext
+    """
     return (ciphertext % key) % 2
 
 
 if __name__ == "__main__":
     security_parameter = 8
     key = generate_key(security_parameter)
-    print(key)
+
     ciphertext_1 = encrypt_bit(security_parameter, key, 0)
-    print(ciphertext_1)
     plaintext_1 = decrypt(ciphertext_1, key)
-    print(plaintext_1)
+
